ac6cc513f33373d7efcfca54ed30440ef29946f8
dingirtorres <dingirtorres3@gmail.com>
2024-09-08 22:57:43 -0300

    Update script.js

--- mayor/js/script.js
+++ mayor/js/script.js
@@ -1,15 +1,15 @@
 
         document.getElementById('numerosForm').addEventListener('submit', function(event) {
-            // Obtener los valores ingresados
+           
             const num1 = Number(document.getElementById('num1').value);
             const num2 = Number(document.getElementById('num2').value);
             const num3 = Number(document.getElementById('num3').value);
             const num4 = Number(document.getElementById('num4').value);
 
-            // Calcular el mayor usando Math.max
+            
             const mayor = Math.max(num1, num2, num3, num4);
 
-            // Mostrar el resultado usando alert
+           
             alert(`El número mayor es: ${mayor}`);
         });
  
